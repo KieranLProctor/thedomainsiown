eb1ddafee6ada9afde06200196b7d370abb0e1f1
Kieran Proctor <kieran.lee.proctor@gmail.com>
2021-08-28 20:13:00 +0100

    Removed unneeded code from old datatable.

--- resources/views/components/input/text.blade.php
+++
@@ -1,1 +0,0 @@
-<input {{ $disabled ? 'disabled' : '' }} type="text" {!! $attributes->merge(['class' => 'shadow-sm focus:ring-gray-500 focus:border-gray-500 block w-full sm:text-sm border-gray-300 rounded-md']) !!}>
